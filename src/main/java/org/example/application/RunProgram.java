25141ad94bec4b7d5bd04af1bf7de8aafeaef1cf
Christakis <banzachristakis@gmail.com>
2024-10-20 20:59:09 -0300

    chess layer and printing thr board

--- src/main/java/org/example/application/RunProgram.java
+++ src/main/java/org/example/application/RunProgram.java
@@ -1,10 +1,14 @@
 package org.example.application;
 
 import org.example.boardGame.Board;
+import org.example.chess.ChessMatch;
 
 
 public class RunProgram {
     public static void main(String[] args) {
-        Board board = new Board(8,8);
+
+        ChessMatch chessMatch = new ChessMatch();
+        UI.printBoard(chessMatch.getPieces());
+
     }
 }
